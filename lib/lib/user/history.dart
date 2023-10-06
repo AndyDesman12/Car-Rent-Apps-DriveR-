aa157b0fc820a03666defd82d9eb6e9a15e43fbd
Blank <101184945+AndyDesman12@users.noreply.github.com>
2023-10-06 17:45:19 +0700

    Add files via upload

---
+++ lib/lib/user/history.dart
@@ -0,0 +1,258 @@
+import 'package:flutter/material.dart';
+import 'homeUser.dart';
+
+
+class History extends StatefulWidget {
+  const History({super.key});
+
+  @override
+  State<History> createState() => _HistoryState();
+}
+
+class _HistoryState extends State<History> {
+  int hargaAvanza = 100000;
+  int stokAvanza = 1;
+  int hargaInnova = 150000;
+  int stokInnova = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Text('History'),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlue[600],
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 40.0, 0, 0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Card(
+                  elevation: 5.0,
+                  color: Colors.white,
+                  shadowColor: Colors.lightBlue,
+                  child: Container(
+                    child: const Image(
+                      image: AssetImage(
+                        'assets/avanza.jpg',
+                      ),
+                      height: 150,
+                      width: 220,
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0),
+                      child: const Text(
+                        'Status',
+                        style: TextStyle(
+                          fontFamily: 'Outfit',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0),
+                      child: const Text(
+                        'Menunggu',
+                        style: TextStyle(
+                          fontFamily: 'Outfit',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30.0,
+                          color: Colors.yellow,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(10.0, 10, 0, 0),
+                          child: Icon(
+                            Icons.people,
+                            color: Colors.black,
+                          ),
+                        ),
+
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(7.0, 10, 0, 0),
+                          child: Text(
+                            'Andy Desman',
+                            style: const TextStyle(
+                              color: Colors.lightBlue,
+                              fontFamily: 'Outfit',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15.0,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(10.0, 10, 0, 0),
+                          child: Icon(
+                            Icons.card_membership,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(7.0, 5, 0, 0),
+                          child: Text(
+                            '1234567890123456',
+                            style: const TextStyle(
+                              color: Colors.lightBlue,
+                              fontFamily: 'Outfit',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15.0,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Card(
+                  margin: const EdgeInsets.fromLTRB(0, 30.0, 0, 0),
+                  elevation: 5.0,
+                  color: Colors.white,
+                  shadowColor: Colors.lightBlue,
+                  child: Container(
+                    child: const Image(
+                      image: AssetImage(
+                        'assets/innova.jpg',
+                      ),
+                      height: 150,
+                      width: 220,
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(20.0, 20.0, 0.0, 0.0),
+                      child: const Text(
+                        'Status',
+                        style: TextStyle(
+                          fontFamily: 'Outfit',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 0.0),
+                      child: const Text(
+                        'Selesai',
+                        style: TextStyle(
+                          fontFamily: 'Outfit',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30.0,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(20.0, 10, 0, 0),
+                          child: Icon(
+                            Icons.people,
+                            color: Colors.black,
+                          ),
+                        ),
+
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(7.0, 10, 0, 0),
+                          child: Text(
+                            'Fiqih',
+                            style: const TextStyle(
+                              color: Colors.lightBlue,
+                              fontFamily: 'Outfit',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15.0,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(20.0, 10, 0, 0),
+                          child: Icon(
+                            Icons.card_membership,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(7.0, 5, 0, 0),
+                          child: Text(
+                            '1234567890789456',
+                            style: const TextStyle(
+                              color: Colors.lightBlue,
+                              fontFamily: 'Outfit',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15.0,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        color: Colors.lightBlue,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.home),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomeUser()),
+                );
+              },
+              color: Colors.white,
+            ),
+            Card(
+              elevation: 20.0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0)
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.history),
+                onPressed: () {},
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/lib/user/homeUser.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+import 'history.dart';
+
+
+class HomeUser extends StatefulWidget {
+  const HomeUser({super.key});
+
+  @override
+  State<HomeUser> createState() => _HomeUserState();
+}
+class _HomeUserState extends State<HomeUser> {
+  int hargaAvanza = 100000;
+  int stokAvanza = 1;
+  int hargaInnova = 150000;
+  int stokInnova = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Text('DriveR'),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlue[600],
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 40.0, 0, 0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Card(
+                  elevation: 5.0,
+                  color: Colors.white,
+                  shadowColor: Colors.lightBlue,
+                  child: Container(
+                    child: const Image(
+                      image: AssetImage(
+                        'assets/avanza.jpg',
+                      ),
+                      height: 150,
+                      width: 220,
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 0.0),
+                      child: const Text(
+                        'Avanza',
+                        style: TextStyle(
+                          fontFamily: 'Outfit',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30.0,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(20.0, 0, 0, 0),
+                      child: Text(
+                        'RP. $hargaAvanza',
+                        style: const TextStyle(
+                          color: Colors.lightBlue,
+                          fontFamily: 'Outfit',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(20.0, 5, 0, 0),
+                      child: Text(
+                        'Stok: $stokAvanza',
+                        style: const TextStyle(
+                          color: Colors.grey,
+                          fontFamily: 'Outfit',
+                          fontWeight: FontWeight.normal,
+                          fontSize: 17.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Card(
+                  margin: const EdgeInsets.fromLTRB(0, 30.0, 0, 0),
+                  elevation: 5.0,
+                  color: Colors.white,
+                  shadowColor: Colors.lightBlue,
+                  child: Container(
+                    child: const Image(
+                      image: AssetImage(
+                        'assets/innova.jpg',
+                      ),
+                      height: 150,
+                      width: 220,
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 0.0),
+                      child: const Text(
+                        'Innova',
+                        style: TextStyle(
+                          fontFamily: 'Outfit',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30.0,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(20.0, 0, 0, 0),
+                      child: Text(
+                        'RP. $hargaInnova',
+                        style: const TextStyle(
+                          color: Colors.lightBlue,
+                          fontFamily: 'Outfit',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(20.0, 5, 0, 0),
+                      child: Text(
+                        'Stok: $stokInnova',
+                        style: const TextStyle(
+                          color: Colors.grey,
+                          fontFamily: 'Outfit',
+                          fontWeight: FontWeight.normal,
+                          fontSize: 17.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        color: Colors.lightBlue,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Card(
+              elevation: 20.0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0)
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.home),
+                onPressed: () {},
+                color: Colors.black,
+              ),
+            ),
+            IconButton(
+              icon: const Icon(Icons.history),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const History()),
+                );
+              },
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
