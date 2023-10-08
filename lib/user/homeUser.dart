8a117583e82f00fd7959f39e9d4de99f8800202d
Blank <101184945+AndyDesman12@users.noreply.github.com>
2023-10-08 21:29:07 +0700

    Add files via upload

--- lib/user/homeUser.dart
+++ lib/user/homeUser.dart
@@ -2,6 +2,28 @@ import 'package:flutter/material.dart';
 import 'history.dart';
 import 'carInfo.dart';
 
+void _showDialog(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Konfirmasi Pemesanan'),
+        content: Text('Anda diminta untuk mengirimkan foto KTP sebagai verifikasi pemesanan'),
+        titleTextStyle: TextStyle(
+          fontFamily: 'Outfit',
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: Text('Lanjut'),
+            onPressed: () {
+              Navigator.of(context).pop(); // Close the dialog
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
 
 class HomeUser extends StatefulWidget {
   const HomeUser({super.key});
@@ -27,7 +49,7 @@ class _HomeUserState extends State<HomeUser> {
 
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 40.0, 0, 0),
+          padding: const EdgeInsets.fromLTRB(0, 30.0, 0, 0),
           child: Column(
             children: [
               Row(
@@ -55,117 +77,155 @@ class _HomeUserState extends State<HomeUser> {
                       ),
                     ),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 0.0),
-                        child: const Text(
-                          'Avanza',
-                          style: TextStyle(
-                            fontFamily: 'Outfit',
-                            fontWeight: FontWeight.bold,
-                            fontSize: 30.0,
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 0.0),
+                          child: const Text(
+                            'Avanza',
+                            style: TextStyle(
+                              fontFamily: 'Outfit',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30.0,
+                            ),
                           ),
                         ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.fromLTRB(20.0, 0, 0, 0),
-                        child: Text(
-                          'RP. $hargaAvanza',
-                          style: const TextStyle(
-                            color: Colors.lightBlue,
-                            fontFamily: 'Outfit',
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20.0,
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(20.0, 0, 0, 0),
+                          child: Text(
+                            'RP. $hargaAvanza',
+                            style: const TextStyle(
+                              color: Colors.lightBlue,
+                              fontFamily: 'Outfit',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.0,
+                            ),
                           ),
                         ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.fromLTRB(20.0, 5, 0, 0),
-                        child: Text(
-                          'Stok: $stokAvanza',
-                          style: const TextStyle(
-                            color: Colors.grey,
-                            fontFamily: 'Outfit',
-                            fontWeight: FontWeight.normal,
-                            fontSize: 17.0,
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(20.0, 5.0, 0, 0),
+                          child: Text(
+                            'Stok: $stokAvanza',
+                            style: const TextStyle(
+                              color: Colors.grey,
+                              fontFamily: 'Outfit',
+                              fontWeight: FontWeight.normal,
+                              fontSize: 17.0,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                        Container(
+                            margin: const EdgeInsets.fromLTRB(20.0, 10.0, 0.0, 0.0),
+                            child: ElevatedButton.icon(
+                              onPressed: () {
+                                _showDialog(context); // Call the function to show the dialog
+                              },
+                              icon: const Icon(Icons.shopping_cart),
+                              label: const Text('Pesan'),
+                              style: ElevatedButton.styleFrom(
+                                foregroundColor: Colors.white, backgroundColor: Colors.green, // Set the text color
+                                elevation: 5, // Set the button's elevation
+                                // You can customize other button properties here
+                              ),
+                            )
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      // Navigate to the second page when the image is tapped
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const carInfo()),
-                      );
-                    },
-                    child: Card(
-                      margin: const EdgeInsets.fromLTRB(0, 30.0, 0, 0),
-                      elevation: 5.0,
-                      color: Colors.white,
-                      shadowColor: Colors.lightBlue,
-                      child: Container(
-                        child: const Image(
-                          image: AssetImage(
-                            'assets/innova.jpg',
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 30.0, 0, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        // Navigate to the second page when the image is tapped
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const carInfo()),
+                        );
+                      },
+                      child: Card(
+                        elevation: 5.0,
+                        color: Colors.white,
+                        shadowColor: Colors.lightBlue,
+                        child: Container(
+                          child: const Image(
+                            image: AssetImage(
+                              'assets/innova.jpg',
+                            ),
+                            height: 150,
+                            width: 220,
                           ),
-                          height: 150,
-                          width: 220,
                         ),
                       ),
                     ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 0.0),
-                        child: const Text(
-                          'Innova',
-                          style: TextStyle(
-                            fontFamily: 'Outfit',
-                            fontWeight: FontWeight.bold,
-                            fontSize: 30.0,
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 0.0),
+                            child: const Text(
+                              'Innova',
+                              style: TextStyle(
+                                fontFamily: 'Outfit',
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30.0,
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.fromLTRB(20.0, 0, 0, 0),
-                        child: Text(
-                          'RP. $hargaInnova',
-                          style: const TextStyle(
-                            color: Colors.lightBlue,
-                            fontFamily: 'Outfit',
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20.0,
+                          Container(
+                            margin: const EdgeInsets.fromLTRB(20.0, 0, 0, 0),
+                            child: Text(
+                              'RP. $hargaInnova',
+                              style: const TextStyle(
+                                color: Colors.lightBlue,
+                                fontFamily: 'Outfit',
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20.0,
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.fromLTRB(20.0, 5, 0, 0),
-                        child: Text(
-                          'Stok: $stokInnova',
-                          style: const TextStyle(
-                            color: Colors.grey,
-                            fontFamily: 'Outfit',
-                            fontWeight: FontWeight.normal,
-                            fontSize: 17.0,
+                          Container(
+                            margin: const EdgeInsets.fromLTRB(20.0, 5, 0, 0),
+                            child: Text(
+                              'Stok: $stokInnova',
+                              style: const TextStyle(
+                                color: Colors.grey,
+                                fontFamily: 'Outfit',
+                                fontWeight: FontWeight.normal,
+                                fontSize: 17.0,
+                              ),
+                            ),
                           ),
-                        ),
+                          Container(
+                              margin: const EdgeInsets.fromLTRB(20.0, 10.0, 0.0, 0.0),
+                              child: ElevatedButton.icon(
+                                onPressed: () {
+                                  _showDialog(context); // Call the function to show the dialog
+                                },
+                                icon: const Icon(Icons.shopping_cart),
+                                label: const Text('Pesan'),
+                                style: ElevatedButton.styleFrom(
+                                  foregroundColor: Colors.white, backgroundColor: Colors.green, // Set the text color
+                                  elevation: 5, // Set the button's elevation
+                                  // You can customize other button properties here
+                                ),
+                              )
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ],
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
